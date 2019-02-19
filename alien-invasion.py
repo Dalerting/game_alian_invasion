e3f4a3f56bf7081be65f6c7e46813508c47ce830
li sheng dan <646286091@qq.com>
2019-02-19 19:23:00 +0800

    Add files via upload
    更新

--- alien-invasion.py
+++ alien-invasion.py
@@ -1,17 +1,20 @@
 import sys
 import pygame
 from settings import Settings
+from ship import Ship
 def run_game():
 #初始化
     pygame.init()
     ai_seetings=Settings()
     screen=pygame.display.set_mode((ai_seetings.screen_width,ai_seetings.screnn_heght))
     pygame.display.set_caption("Alien Invasion")
+    ship = Ship(screen)
     #开始主循环
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
         screen.fill(ai_seetings.bg_color)
+        ship.blitme()
         pygame.display.flip()
 run_game()
--- settings.py
+++ settings.py
@@ -1,5 +1,5 @@
 class Settings():
     def __init__(self):
-        self.screen_width=1200
-        self.screnn_heght=800
+        self.screen_width=900
+        self.screnn_heght=600
         self.bg_color=(230,230,230)
