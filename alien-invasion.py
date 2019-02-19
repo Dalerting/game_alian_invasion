17391d3390d27e141525be44873c22ece87c72f5
Dalerting <646286091@qq.com>
2019-02-19 20:48:33 +0800

    添加子弹功能

--- alien-invasion.py
+++ alien-invasion.py
@@ -1,5 +1,6 @@
 import pygame
 
+from pygame.sprite import Group
 from settings import Settings
 from ship import Ship
 import game_function as gf
@@ -7,15 +8,16 @@ import game_function as gf
 def run_game():
 #初始化
     pygame.init()
-    ai_seetings = Settings()
-    screen = pygame.display.set_mode((ai_seetings.screen_width, ai_seetings.screnn_heght))
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
-    ship = Ship(screen)
+    ship = Ship(ai_settings, screen)
+    bullets = Group()
     #开始主循环
     while True:
-        gf.check_events()
+        gf.check_events(ai_settings, screen, ship, bullets)
+        ship.update()
+        gf.update_screen(ai_settings, screen, ship, bullets)
 
-        screen.fill(ai_seetings.bg_color)
-        ship.blitme()
-        pygame.display.flip()
+
 run_game()
